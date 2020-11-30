787bedeaa09439a0a11ac328f34022b346763c6c
Remy <r.zyngfogel@calypso.science>
2020-11-30 16:14:32 +1300

    add mercator inpout

--- pre-processing/core/datasourcing.py
+++ pre-processing/core/datasourcing.py
@@ -67,6 +67,7 @@ class download_data(object):
 
             for itry in range(0,TRY):
                 self.logger.info('Try #%i for %s' % (itry,var))
+                prin(url)
                 os.system(url)
                 if os.path.isfile(os.path.join(root,var+'_'+t0.strftime('%Y%m%d%H%M00')+'.nc')):
                     break
