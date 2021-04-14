0173125903a021c13125fd67fccfbe0b6f855287
Remy Zyngfogel <remy@system76-pc.localdomain>
2021-04-14 14:23:25 +1200

    lon,lat

--- pre-processing/core/ic.py
+++ pre-processing/core/ic.py
@@ -86,7 +86,7 @@ class InitialConditions(object):
         #time0=[np.datetime64(x) for x in time0]
 
         geo_idx = (np.abs(date2num(time0)-date2num(self.t0))).argmin() # closest timestep
-        import pdb;pdb.set_trace()
+       
         varin=data[var][geo_idx]
         if len(varin.shape)>2:
             varin=varin[0] # get surface level
