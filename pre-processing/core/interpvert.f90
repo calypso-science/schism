89aa423f25a96fe2d48d39e1f96906f5ccb63a37
Remy Zyngfogel <remy@system76-pc.localdomain>
2021-04-14 17:02:23 +1200

    interp test

--- pre-processing/core/interpvert.f90
+++ pre-processing/core/interpvert.f90
@@ -1,53 +1,63 @@
-! -*- f90 -*-
-
-!f2py -m interpvert -h interpvert.pyf interpvert.f90
+!
+! ++++++++++++++++++++++++++++++++++++++++++++++++++++
+! copied from selfe2netcdf interpolatez subroutine 
+! Please keep in sync!
+! ++++++++++++++++++++++++++++++++++++++++++++++++++++
+!
+!Generate wrapper by calling
+!f2py -m interpz -h interpz.pyf interpz.f90
 !Generate library
-!f2py  -c interpvert.pyf  interpvert.f90
+!f2py  -c interpz.pyf  interpz.f90
 
-      subroutine interph(var,px,py,x0,y0,idx,idy,vout,np,nx0,ny0)
-      integer np,ip,ii,jj,nx0,ny0
-      real var(ny0,nx0),vout(np)
-      real px(np),py(np),idx,idy
-      real sdx,sdy,sdx1,sdy1,ppx,ppy,fac,tmpint,tmp
+subroutine interpz1d(varin, zin, zout, np, nzin, nzout, kz, null_value, varout)
 
-      do ip=1,np
-        ppx=idx*(px(ip)-x0)+1.
-        ppy=idy*(py(ip)-y0)+1.
-!        print*,px(ip),py(ip),ppx,ppy
-        tmpint=0.
-        fac=0.
-        sdx=mod(ppx,1.0)
-        sdy=mod(ppy,1.0)
-        sdx1=(1.-sdx)
-        sdy1=(1.-sdy)
-        ii=min(max(1,int(ppx)),nx0)
-        jj=min(max(1,int(ppy)),ny0)
-        if (abs(var(jj,ii)).lt.9999) then
-          tmp=sdx1*sdy1
-          tmpint=tmpint+tmp*var(jj,ii)
-          fac=fac+tmp
-        endif
-        if (jj.lt.ny0.and.abs(var(jj+1,ii)).lt.9999) then
-          tmp=sdx1*sdy
-          tmpint=tmpint+tmp*var(jj+1,ii)
-          fac=fac+tmp
-        endif
-        if (ii.lt.nx0.and.abs(var(jj,ii+1)).lt.9999) then
-          tmp=sdx*sdy1
-          tmpint=tmpint+tmp*var(jj,ii+1)
-          fac=fac+tmp
-        endif
-        if (ii.lt.nx0.and.jj.lt.ny0.and.abs(var(jj+1,ii+1)).lt.9999) then
-          tmp=sdx*sdy
-          tmpint=tmpint+tmp*var(jj+1,ii+1)
-          fac=fac+tmp
-        endif
-        if (fac.gt.0) then
-          tmpint=tmpint/fac
-        else
-          tmpint=0.
-        endif
-        vout(ip)=tmpint
-      enddo
-      return
-      end subroutine
+
+    integer                     , intent(in)        :: np, nzin, nzout, kz
+    real(8)                     , intent(in)        :: null_value
+    real(8), dimension(np,nzin) , intent(in)        :: zin , varin
+    real(8), dimension(nzout)   , intent(in)        :: zout
+    real(8), dimension(np,nzout), intent(out)       :: varout
+    ! Locals 
+    integer                                         :: i,k,k0
+    real(8)                                         :: rat
+
+    real, parameter     :: almost_zero = 1.e-6
+
+
+    varout=null_value
+
+    do i=1,np
+
+      if (abs(zin(i,kz)) <= almost_zero .and. abs(zin(i,nzin)) <= almost_zero) cycle ! dry nodes have all zins =0.
+
+
+
+      ! interpolate z
+      if (zout(i) < zin(i,kz)) then ! zout below bathymetry
+        ! do nothing
+      elseif (zout(i) >= zin(i,nzin)) then
+        !if (debug .and. i==debug_node) write(*,*) 'getting surface!'
+        write(*,*) 'getting surface!'
+        varout(i,i) = varin(i,nzin)
+      else
+
+        ! interp1d
+        k0=-1! layer below
+        do k=kz,nzin-1
+          if (zin(i,k) <= zout(i) .and. zout(i) < zin(i,k+1)) then
+            k0=k
+            exit
+          endif
+        enddo !k
+
+
+        rat = (zout(i)-zin(i,k0)) / (zin(i,k0+1)-zin(i,k0))
+        varout(i,i) = varin(i,k0)*(1.-rat)+varin(i,k0+1)*rat
+
+      endif
+
+
+    enddo !i=1,np
+
+
+  end subroutine
